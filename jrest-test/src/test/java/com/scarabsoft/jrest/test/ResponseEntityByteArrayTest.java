2c34d263e8838fd57c716fd6dd55f01197909562
Dominique Dymke <admin@scarabsoft.com>
2015-12-05 17:58:29 +0100

    extend Test

--- jrest-test/src/test/java/com/scarabsoft/jrest/test/ResponseEntityByteArrayTest.java
+++ jrest-test/src/test/java/com/scarabsoft/jrest/test/ResponseEntityByteArrayTest.java
@@ -8,6 +8,7 @@ import com.scarabsoft.jrest.annotation.Put;
 import com.scarabsoft.jrest.converter.GsonConverterFactory;
 import com.scarabsoft.jrest.converter.exception.StringExceptionConverterFactory;
 import com.scarabsoft.jrest.interceptor.ResponseEntity;
+import com.scarabsoft.jrest.util.IOUtils;
 import org.hamcrest.Matchers;
 import org.junit.Assert;
 import org.junit.Before;
@@ -17,6 +18,9 @@ import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = JRestTestApplication.class)
@@ -56,11 +60,45 @@ public class ResponseEntityByteArrayTest {
         assertion(app.DELETE());
     }
 
+    @Test
+    public void InputStreamGETTest() {
+        assertionStream(app.iGET());
+    }
+
+    @Test
+    public void InputStreamPOSTTest() {
+        assertionStream(app.iPOST());
+    }
+
+    @Test
+    public void InputStreamPUTTest() {
+        assertionStream(app.iPUT());
+    }
+
+    @Test
+    public void InputStreamDELETETest() {
+        assertionStream(app.iDELETE());
+    }
+
     private void assertion(ResponseEntity<byte[]> res) {
         Assert.assertThat(res.getStatusCode(), Matchers.is(200));
         Assert.assertThat(res.getObject().length, Matchers.is(10));
         for (int i = 0; i < 10; i++) {
             Assert.assertThat(res.getObject()[i], Matchers.is((byte) 1));
+        }
+    }
+
+    private void assertionStream(ResponseEntity<InputStream> res) {
+        byte[] data = new byte[0];
+        try {
+            data = IOUtils.convert(res.getObject());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Assert.assertThat(res.getStatusCode(), Matchers.is(200));
+        Assert.assertThat(data.length, Matchers.is(10));
+        for (int i = 0; i < 10; i++) {
+            Assert.assertThat(data[i], Matchers.is((byte) 1));
         }
     }
 
@@ -78,6 +116,18 @@ public class ResponseEntityByteArrayTest {
 
         @Delete
         ResponseEntity<byte[]> DELETE();
+
+        @Get
+        ResponseEntity<InputStream> iGET();
+
+        @Post
+        ResponseEntity<InputStream> iPOST();
+
+        @Put
+        ResponseEntity<InputStream> iPUT();
+
+        @Delete
+        ResponseEntity<InputStream> iDELETE();
     }
 
 }
